e49612908819aeaac9d3f9944e7b04d69d2800a1
Shadow318193 <shadow318193@gmail.com>
2022-05-07 15:49:47 +0300

    Changes for pythonanywhere.com

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, render_template, redirect
+
 from captcha.image import ImageCaptcha
+from clean_captcha import clean_captcha
+
 from random import choice
+
 from threading import Timer
 
 import os
+
 import datetime
 
 from data import db_session
 from data.boards import Boards
 from data.posts import Posts
-from clean_captcha import clean_captcha
 
+# CONFIGURATION
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/media/from_users'
@@ -20,9 +25,15 @@ IMAGE_FILES = ["png", "jpg", "jpeg", "gif", "jfif", "pjpeg", "pjp", "jpe", "webp
 VIDEO_FILES = ["webm", "mp4", "m4v"]
 AUDIO_FILES = ["mp3", "wav"]
 
+PICS_404 = ["crab-rave.gif", "mario.gif"]
+PICS_413 = ["too_cool_and_dangerous.gif", "over9000.png"]
+
 ALLOWED_FILES = IMAGE_FILES + VIDEO_FILES + AUDIO_FILES
 
+PYTHONANYWHERE = False  # http://shadow318193.pythonanywhere.com
 
+
+# Code start
 class RepeatTimer(Timer):
     def __init__(self, interval, function, args=None, kwargs=None):
         Timer.__init__(self, interval=interval, function=function, args=args, kwargs=kwargs)
@@ -33,6 +44,7 @@ class RepeatTimer(Timer):
 
 
 def make_accept_for_html(mime):
+    # For input tag in HTML
     if mime in IMAGE_FILES:
         return "image/" + mime
     elif mime in VIDEO_FILES:
@@ -42,6 +54,8 @@ def make_accept_for_html(mime):
 
 
 accept = ",".join([make_accept_for_html(x) for x in ALLOWED_FILES])
+
+# For correct work of captcha
 captcha_for_ip = {}
 
 
@@ -50,10 +64,14 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_FILES
 
 
-def generate_new_captcha(ip):
+def clear_captcha_from_ip(ip):
     if ip in captcha_for_ip:
         if os.path.isfile(f"static/media/captchas/{captcha_for_ip[ip]}.png"):
             os.remove(f"static/media/captchas/{captcha_for_ip.pop(ip)}.png")
+
+
+def generate_new_captcha(ip):
+    clear_captcha_from_ip(ip)
     captcha_for_ip[ip] = "".join([choice(
         ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) for _ in range(6)])
     image = ImageCaptcha(width=280, height=90)
@@ -62,12 +80,16 @@ def generate_new_captcha(ip):
     print("Сгенерирована новая каптча")
 
 
+def get_ip():
+    if PYTHONANYWHERE:
+        return request.headers['X-Real-IP']
+    return request.remote_addr
+
+
 @app.route("/")
 def index():
-    ip = request.remote_addr
-    if ip in captcha_for_ip:
-        if os.path.isfile(f"static/media/captchas/{captcha_for_ip[ip]}.png"):
-            os.remove(f"static/media/captchas/{captcha_for_ip.pop(ip)}.png")
+    ip = get_ip()
+    clear_captcha_from_ip(ip)
     db_sess = db_session.create_session()
     boards = db_sess.query(Boards)
     posts = {}
@@ -78,20 +100,18 @@ def index():
 
 @app.route("/robots.txt")
 def robots():
-    ip = request.remote_addr
-    if ip in captcha_for_ip:
-        if os.path.isfile(f"static/media/captchas/{captcha_for_ip[ip]}.png"):
-            os.remove(f"static/media/captchas/{captcha_for_ip.pop(ip)}.png")
+    ip = get_ip()
+    clear_captcha_from_ip(ip)
     return f"<pre>{open('robots.txt', 'r').read()}</pre>"
 
 
 @app.route("/<board_name>", methods=['POST', 'GET'])
 def board_url(board_name):
-    ip = request.remote_addr
-    # ip = request.headers['X-Real-IP']
-    # FOR PYTHONANYWHERE.COM
+    ip = get_ip()
+
     if request.method == 'GET':
         generate_new_captcha(ip)
+
         db_sess = db_session.create_session()
         board_select = db_sess.query(Boards).filter(Boards.name == board_name)
         for board_obj in board_select:
@@ -100,13 +120,15 @@ def board_url(board_name):
             post_answers = {}
             for post in posts:
                 post_answers[post.id] = db_sess.query(Posts).filter(Posts.parent_post == post.id).count()
+
             return render_template("board.html", board=board_obj, posts=posts, posts_count=posts.count(),
                                    post_answers=post_answers, image_files=IMAGE_FILES, video_files=VIDEO_FILES,
                                    audio_files=AUDIO_FILES, accept_files=accept,
                                    captcha=f"/static/media/captchas/{captcha_for_ip[ip]}.png")
+
         return render_template("error.html", code=404,
                                text="К сожалению, данная доска больше недоступна или её не существует.",
-                               pics=["crab-rave.gif", "mario.gif"])
+                               pics=PICS_404)
     elif request.method == 'POST':
         if "like" in request.form:
             db_sess = db_session.create_session()
@@ -120,7 +142,7 @@ def board_url(board_name):
                         p.raters = ";".join(raters)
                     else:
                         p.raters = ip
-                    print(f"С устройства с IP {ip} был оставлен лайк на посте с ID {p.id}:")
+                    print(f"С устройства под IP {ip} был оставлен лайк посту под ID {p.id}")
             db_sess.commit()
             return redirect(board_name)
         elif "dislike" in request.form:
@@ -135,12 +157,14 @@ def board_url(board_name):
                         p.raters = ";".join(raters)
                     else:
                         p.raters = ip
-                    print(f"С устройства с IP {ip} был оставлен дизлайк на посте с ID {p.id}:")
+                    print(f"С устройства под IP {ip} был оставлен дизлайк посту под ID {p.id}")
             db_sess.commit()
             return redirect(board_name)
+
         if not (request.form["topic"] or request.form["text"] or request.files["file"]) \
                 or request.form["captcha"] != captcha_for_ip[ip]:
             return redirect(board_name)
+
         post = Posts()
         post.time = datetime.datetime.now()
         post.poster = ip
@@ -158,23 +182,25 @@ def board_url(board_name):
         db_sess = db_session.create_session()
         db_sess.add(post)
         db_sess.commit()
-        print(f"С устройства с IP {ip} было отправлено сообщение следующего содержания:")
+
+        print(f"С устройства под IP {ip} было отправлено сообщение следующего содержания:")
         print(f"ID поста: {post.id}")
         print(f"Заголовок: {post.topic}")
         print(f"Основной текст: {post.text}")
         print(f"Медиа: {post.media_name}")
         print(f"Время: {post.time}")
+
         generate_new_captcha(ip)
         return redirect(board_name + "/" + str(post.id))
 
 
 @app.route("/<board_name>/<post_id>", methods=['POST', 'GET'])
 def post_url(board_name, post_id):
-    ip = request.remote_addr
-    # ip = request.headers['X-Real-IP']
-    # FOR PYTHONANYWHERE.COM
+    ip = get_ip()
+
     if request.method == 'GET':
         generate_new_captcha(ip)
+
         db_sess = db_session.create_session()
         board_select = db_sess.query(Boards).filter(Boards.name == board_name)
         post_select = db_sess.query(Posts).filter(Posts.id == post_id,
@@ -183,13 +209,15 @@ def post_url(board_name, post_id):
         for board_obj in board_select:
             for post_obj in post_select:
                 posts = db_sess.query(Posts).filter(Posts.parent_post == post_id)
+
                 return render_template("post.html", board=board_obj, the_post=post_obj, posts=posts,
                                        posts_count=posts.count(), image_files=IMAGE_FILES, video_files=VIDEO_FILES,
                                        audio_files=AUDIO_FILES, accept_files=accept,
                                        captcha=f"/static/media/captchas/{captcha_for_ip[ip]}.png")
+
         return render_template("error.html", code=404,
                                text="К сожалению, данный тред больше недоступен или его не существует.",
-                               pics=["crab-rave.gif", "mario.gif"])
+                               pics=PICS_404)
     elif request.method == 'POST':
         if "like" in request.form:
             db_sess = db_session.create_session()
@@ -203,7 +231,7 @@ def post_url(board_name, post_id):
                         p.raters = ";".join(raters)
                     else:
                         p.raters = ip
-                    print(f"С устройства с IP {ip} был оставлен лайк на посте с ID {p.id}:")
+                    print(f"С устройства под IP {ip} был оставлен лайк посту под ID {p.id}")
             db_sess.commit()
             return redirect(post_id)
         elif "dislike" in request.form:
@@ -218,12 +246,14 @@ def post_url(board_name, post_id):
                         p.raters = ";".join(raters)
                     else:
                         p.raters = ip
-                    print(f"С устройства с IP {ip} был оставлен дизлайк на посте с ID {p.id}:")
+                    print(f"С устройства под IP {ip} был оставлен дизлайк посту под ID {p.id}")
             db_sess.commit()
             return redirect(post_id)
+
         if not (request.form["topic"] or request.form["text"] or request.files["file"]) \
                 or request.form["captcha"] != captcha_for_ip[ip]:
             return redirect(post_id)
+
         post = Posts()
         post.time = datetime.datetime.now()
         post.poster = ip
@@ -242,36 +272,44 @@ def post_url(board_name, post_id):
         db_sess = db_session.create_session()
         db_sess.add(post)
         db_sess.commit()
-        print(f"С устройства с IP {ip} было отправлено сообщение следующего содержания:")
+
+        print(f"С устройства под IP {ip} было отправлено сообщение следующего содержания:")
         print(f"ID поста: {post.id}")
         print(f"Заголовок: {post.topic}")
         print(f"Основной текст: {post.text}")
         print(f"Медиа: {post.media_name}")
         print(f"Время: {post.time}")
-        print(f"Является ответом на пост с ID {post.parent_post}")
+        print(f"Является ответом на пост под ID {post.parent_post}")
+
         generate_new_captcha(ip)
         return redirect(post_id)
 
 
 @app.errorhandler(404)
 def error404(e):
+    ip = get_ip()
+    clear_captcha_from_ip(ip)
     print(e)
     return render_template("error.html", code=404,
                            text="К сожалению, данный материал больше недоступен или его не существует.",
-                           pics=["crab-rave.gif", "mario.gif"])
+                           pics=PICS_404)
 
 
 @app.errorhandler(413)
 def error413(e):
+    ip = get_ip()
+    clear_captcha_from_ip(ip)
     print(e)
     return render_template("error.html", code=413,
                            text="Извините, но прикреплённый файл оказался по размерам слишком крут "
                                 "и опасен для сервера, поэтому сервер подписал отказ в отправке.",
-                           pics=["too_cool_and_dangerous.gif", "over9000.png"])
+                           pics=PICS_413)
 
 
 @app.errorhandler(500)
 def error500(e):
+    ip = get_ip()
+    clear_captcha_from_ip(ip)
     print(e)
     return render_template("error.html", code=500,
                            text="По причинческим технинам сервер подписал отказ. "
@@ -279,26 +317,17 @@ def error500(e):
                            pics=["prichincheskaya_tehnina.png"])
 
 
-@app.errorhandler(502)
-def error502(e):
-    print(e)
-    return render_template("error.html", code=502,
-                           text="По причинческим технинам у сервера разногласия с хостом. "
-                                "Извините за доставленные неудобства.",
-                           pics=["prichincheskaya_tehnina.png"])
+if PYTHONANYWHERE:
+    def main():
+        db_session.global_init("db/imageboard.db")
+        clean_captcha()
+        timer = RepeatTimer(2 * 60, clean_captcha)
+        timer.start()
+        app.run(host="0.0.0.0")
 
 
-def main():
+    if __name__ == '__main__':
+        main()
+else:
     db_session.global_init("db/imageboard.db")
     clean_captcha()
-    timer = RepeatTimer(2 * 60, clean_captcha)
-    timer.start()
-    app.run(host="0.0.0.0")
-
-
-if __name__ == '__main__':
-    main()
-
-# db_session.global_init("db/imageboard.db")
-# clean_captcha()
-# FOR PYTHONANYWHERE.COM
